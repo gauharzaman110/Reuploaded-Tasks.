dd954244a2c74f39609e7cc6190861f601370312
gauharzaman110 <142487253+gauharzaman110@users.noreply.github.com>
2023-11-12 12:16:15 +0500

    Add files via upload

---
+++ task10a.js
@@ -0,0 +1,14 @@
+// Program by [Your Name], [Current Date]
+// This program demonstrates how to store a person's name in a variable
+// and print the name in lowercase, uppercase, and titlecase.
+
+let personName = "Gauhar";
+
+// Print in lowercase
+console.log(personName.toLowerCase());
+
+// Print in uppercase
+console.log(personName.toUpperCase());
+
+// Print in titlecase
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ task10b.js
@@ -0,0 +1,8 @@
+// Program by [Your Name], [Current Date]
+// This program prints a famous quote and its author.
+
+let famous_person = "Albert Einstein";
+let message = `${famous_person} once said, "A person who never made a mistake never tried anything new."`;
+
+// Print the message
+console.log(message);
